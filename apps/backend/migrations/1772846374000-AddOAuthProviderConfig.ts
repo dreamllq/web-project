5f2d5238a3d6e25bfb9846f21afe184e477586e3
lvliqi <lvliqi_d@163.com>
2026-03-16 22:57:16 +0800

    feat: up

--- apps/backend/migrations/1772846374000-AddOAuthProviderConfig.ts
+++ apps/backend/migrations/1772846374000-AddOAuthProviderConfig.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner, Table, TableIndex } from 'typeorm';
 
-export class AddOAuthProviderConfig1772846374 implements MigrationInterface {
+export class AddOAuthProviderConfig1772846374000 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
